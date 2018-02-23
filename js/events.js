9ecf59f787ca1e568151c7714b6c7669e56b81b6
samory harris <samory070@gmail.com>
2018-02-23 17:05:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,4 +15,25 @@ $(document).ready(function(){
       alert('enter was pressed')
     }
   })
+
+  $(document).on('keyup', function(key) {
+    if (key.which == 82){
+      alert('r was pressed');
+    }
+  });
+
+  $(document).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+});
+
+  $("form").on("submit", function() {
+    if ($( "input:first" ).val() === "correct") {
+      alert('your form is going to be submitted now');
+      return;
+    }
+    alert("you entered the wrong value");
+    return;
+  });
 });

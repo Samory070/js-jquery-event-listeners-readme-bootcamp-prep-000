70b75c26e0d225654644fe9cf1fc273e549fb4da
samory harris <samory070@gmail.com>
2018-02-23 17:41:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -51,16 +51,3 @@ function frameIt() {
     $('img').addClass('tasty')
   })
 }
-
-function pressIt(key) {
-  $('form').on('keydown', function(key) {
-    if(key.which == 71){
-        alert('g was pressed');
-  })
-}
-
-function submitIt() {
-  $('form').on('submit', function() {
-    alert('Your form is going to be submitted now.')
-  })
-}

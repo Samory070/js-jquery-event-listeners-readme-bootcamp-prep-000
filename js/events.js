cdcd669bb5d579b91e7ee4ed4fbc1236a3f8ecf9
samory harris <samory070@gmail.com>
2018-02-23 17:11:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -28,12 +28,12 @@ $(document).ready(function(){
   }
 });
 
-  $("form").on("submit", function() {
-    if ($( "input:first" ).val() === "correct") {
+  $('form').on('submit', function() {
+    if ($( 'input:first' ).val() === 'correct') {
       alert('your form is going to be submitted now');
       return;
     }
-    alert("you entered the wrong value");
+    alert('you entered the wrong value');
     return;
   });
 });
